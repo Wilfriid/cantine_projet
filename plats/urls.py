18f29049677f882a7bc9bd525f2d6272fd54c6e6
wilfriid <kouameezekiel29@gmail.com>
2024-09-05 23:41:25 +0000

    mise a jour

--- plats/urls.py
+++ plats/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-
-from.views import plat , plat_form
+from.views import plat , plat_form ,edit_plat,delete_plat
 
 
 app_name = 'plat'
@@ -8,7 +7,11 @@ app_name = 'plat'
 urlpatterns = [
     path('', plat, name='index'),
     path('plat_form/', plat_form, name='add'),
+    path('edit_plat/<int:id>/', edit_plat, name='edit'),
+    path('delete_plat/<int:id>/',delete_plat, name='delete'),
+]
+
+    
     
    
 
-]
